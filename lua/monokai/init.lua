42818b47d191ed051929c16ea6df015a04fee5c0
Miguel Albernaz <albernazmiguel@gmail.com>
2022-07-07 15:33:56 -0300

    update logic to use native nvim api to create hl groups

--- lua/monokai/init.lua
+++ lua/monokai/init.lua
@@ -1,14 +1,5 @@
 local palette = require("monokai.palette")
 
-local function highlight(group, color)
-  local style = color.style and "gui=" .. color.style or "gui=NONE"
-  local fg = color.fg and "guifg=" .. color.fg or "guifg=NONE"
-  local bg = color.bg and "guibg=" .. color.bg or "guibg=NONE"
-  local sp = color.sp and "guisp=" .. color.sp or ""
-
-  vim.cmd("highlight " .. group .. " " .. style .. " " .. fg .. " " .. bg .. " " .. sp)
-end
-
 local function load_syntax(p)
   return {
     Normal = { fg = p.white, bg = p.base2 },
@@ -238,7 +229,7 @@ M.setup = function(config)
   local syntax = vim.tbl_deep_extend("keep", config.custom_hlgroups, load_syntax(palette))
 
   for group, colors in pairs(syntax) do
-    highlight(group, colors)
+    vim.api.nvim_set_hl(0, group, colors or {})
   end
 
   local async_load_plugin = nil
@@ -250,7 +241,7 @@ M.setup = function(config)
     )
 
     for group, colors in pairs(plugin_syntax) do
-      highlight(group, colors)
+      vim.api.nvim_set_hl(0, group, colors or {})
     end
 
     async_load_plugin:close()
